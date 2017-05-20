6ae3e6f77a5e598221b9f2a3d7eb9ae5e0a199fb
Benjamin Uijldert <bmuijldert@gmail.com>
2017-05-20 18:33:39 +0200

    first commit solved some issues

---
+++ Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour
+{
+    public delegate void AButtonAction();
+    public static event AButtonAction OnAButton;
+    public delegate void DButtonAction();
+    public static event DButtonAction OnDButton;
+
+    void Update ()
+    {
+		if(Input.GetKey(KeyCode.A))
+        {
+            if(OnAButton != null)
+            {
+                OnAButton();
+            }
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            if (OnDButton != null)
+            {
+                OnDButton();
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed;
+
+    private void OnEnable()
+    {
+        KeyboardInput.OnAButton += MoveLeft;
+        KeyboardInput.OnDButton += MoveRight;
+    }
+
+    private void MoveLeft()
+    {
+        transform.Translate(-Vector2.left * Time.deltaTime * _moveSpeed);
+        transform.rotation = Quaternion.Euler(0, 180, 0);
+    }
+
+    private void MoveRight()
+    {
+        transform.Translate(Vector2.right * Time.deltaTime * _moveSpeed);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    private void OnDisable()
+    {
+        KeyboardInput.OnAButton -= MoveLeft;
+        KeyboardInput.OnDButton -= MoveRight;
+    }
+}
